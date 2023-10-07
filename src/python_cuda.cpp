be8d0404999559e90dbb38f4c1e279131e8e3981
Botond Osváth <osvathbotond25@gmail.com>
2023-10-07 14:42:22 +0200

    Improved python interface

--- src/python_cuda.cpp
+++ src/python_cuda.cpp
@@ -10,15 +10,17 @@
 
 
 PYBIND11_MODULE(python_cuda, m) {
-    pybind11::class_<Vector>(m, "Vector")
-        .def(pybind11::init<pybind11::array_t<float> &, bool>(), pybind11::arg("array"), pybind11::arg("copy") = false)
-        .def("get_array", &Vector::getArray)
-        .def("device2host", &Vector::device2Host)
-        .def("host2device", &Vector::host2Device)
-        .def("norm", &vectorNorm)
+    pybind11::class_<Vector>(m, "Vector", "1 dimensional vector of floats with CUDA support.")
+        .def(pybind11::init<pybind11::array_t<float> &, bool>(), "By default, it will use the same memory for the device-side array as the input array. You can avoid it by setting copy=True.", pybind11::arg("array"), pybind11::arg("copy") = false)
+        .def("get_array", &Vector::getArray, "Returns the data in numpy.ndarray. Note that it might be out-of-sync with the actual, GPU array if you modified that.")
+        .def("device2host", &Vector::device2Host, "Copy the data from the device (GPU) to the host (CPU). Equivalent to gpu2cpu.")
+        .def("gpu2cpu", &Vector::device2Host, "Copy the data from the device (GPU) to the host (CPU). Equivalent to device2host.")
+        .def("host2device", &Vector::host2Device, "Copy the data from the host (CPU) to the device (GPU). Equivalent to cpu2gpu.")
+        .def("cpu2gpu", &Vector::host2Device, "Copy the data from the host (CPU) to the device (GPU). Equivalent to host2device.")
+        .def("norm", &vectorNorm, "Calculate the p-norm of the vector.", pybind11::arg("p") = 2)
         .def("__add__", &vectorAdd)
         .def("__sub__", &vectorSub)
-        .def("add", &Vector::add);
+        .def("add", &Vector::add, "Add the vector vec inplace.", pybind11::arg("vec"));
 
     pybind11::register_exception<NumpyShapeError>(m, "NumpyShapeError");
     pybind11::register_exception<NumpyTypeError>(m, "NumpyTypeError");
@@ -27,5 +29,4 @@ PYBIND11_MODULE(python_cuda, m) {
     pybind11::register_exception<CudaMallocError>(m, "CudaMallocError");
     pybind11::register_exception<CudaKernelError>(m, "CudaKernelError");
     pybind11::register_exception<CudaFreeError>(m, "CudaFreeError");
-
 }
